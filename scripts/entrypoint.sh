32e37ac971728ad3aae7c18701d737d7a70aca7d
Rajeesh K V <rajeeshkva2z@gmail.com>
2026-07-26 13:26:24 +0530

    Fixed

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
 set -euo pipefail
-echo "Running EF Core database migrations..."
-dotnet KromicStore.API.dll --migrate-only
-echo "EF Core database migrations completed."
+echo "Starting KromicStore API application (migrations run automatically on startup)..."
+dotnet KromicStore.API.dll
